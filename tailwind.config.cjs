30bcc7302ac6c662cd07fd5c2321db7a0e9df3b7
Bojan Vreco <bojanvreco069@gmail.com>
2022-12-14 19:03:43 +0100

    Why Us section finished

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,19 +8,26 @@ module.exports = {
       },
       fontSize: {
         sm: ['1rem', '22px'],
-        base: ['1.125rem', '24px'],
+        base: ['1.15rem', '30px'],
         lg: ['1.25rem', '26px'],
-        xl: ['1.5rem', '32px'],
+        h3: ['1.5rem', '24px'],
+        h3_md: ['1.625rem', '26px'],
+        h2_md: ['2rem', '22px'],
+        xl: ['2.25rem', '42px'],
         xxl: ['2.5rem', '32px'],
+        h1_md: ['3rem', '38px'],
+        h1_lg: ['3.5rem', '42px'],
         h1: ['3.75rem', '44px'],
       },
       colors: {
         'clr-green': 'hsl(100, 97%, 37%);',
-        'clr-green--dark': 'hsl(100, 97%, 37%);',
-        'clr-blue': 'hsl(200, 99%, 45%);',
+        'clr-green--dark': 'hsl(100, 97%, 32%);',
+        'clr-blue': 'hsl(210,86%,42%)',
         'clr-blue--dark': 'hsl(214, 100%, 29%);',
         'clr-text': 'hsl(0, 0%, 13%);',
         'clr-error': 'hsl(21, 100%, 51%)',
+        'clr-italic': 'hsl(0, 0%, 89%)',
+        'clr-italic--dark': 'hsl(0, 0%, 60%)',
       },
       spacing: {
         'container-padding': '7.5vw',
@@ -34,7 +41,30 @@ module.exports = {
         'hero-home':
           "linear-gradient(to right bottom, rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url('../src/imgs/home/hero.jpg')",
       },
+      borderRadius: {
+        lg: '25px',
+      },
+      boxShadow: {
+        'card-shadow': '0px 0px 3px 1px rgb(0 0 0 / 18%)',
+      },
+      backgroundColor: {
+        'opacity-white': 'rgba(255,255,255,0.1)',
+      },
+      translate: {
+        'negative-50': '-50%',
+        'negative-100': '-100%',
+        'negative-50vw': '-50vw',
+        'negative-100vw': '-100vw',
+      },
+      maxWidth: {
+        'rem-100': '100rem',
+        'rem-80': '80rem',
+      },
+      maxHeight: {
+        'rem-40': '30rem',
+      },
     },
   },
   plugins: [],
 };
+//
