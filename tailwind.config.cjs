9eb46ffb302be777fe677af2b8cda1d574ee2ba9
Bojan Vreco <bojanvreco069@gmail.com>
2022-12-31 13:44:32 +0100

    home finished + services started (hero done)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,10 +10,12 @@ module.exports = {
         sm: ['1rem', '22px'],
         base: ['1.15rem', '30px'],
         lg: ['1.25rem', '26px'],
+        h4: ['1.325rem', '26px'],
         h3: ['1.5rem', '24px'],
-        h3_md: ['1.625rem', '26px'],
+        h3_md: ['1.625rem', '36px'],
+        h2_sm: ['1.75rem', '28px'],
         h2_md: ['2rem', '22px'],
-        xl: ['2.25rem', '42px'],
+        xl: ['2.25rem', '36px'],
         xxl: ['2.5rem', '32px'],
         h1_md: ['3rem', '38px'],
         h1_lg: ['3.5rem', '42px'],
@@ -22,50 +24,51 @@ module.exports = {
       colors: {
         'clr-green': 'hsl(100, 97%, 37%)',
         'clr-green--dark': 'hsl(100, 97%, 32%)',
-        'clr-green--light': 'hsl(100, 97%, 96%)',
+        'clr-green--light': 'hsl(105, 25%, 97%)',
         'clr-blue': 'hsl(210,86%,42%)',
+        'clr-blue--light': 'hsl(200, 99%, 45%)',
         'clr-blue--dark': 'hsl(214, 100%, 29%)',
         'clr-text': 'hsl(0, 0%, 13%)',
-        'clr-error': 'hsl(21, 100%, 51%)',
+        'clr-error': 'hsl(0, 90%,55%)',
+        'clr-error--light': 'hsl(0, 90%,80%)',
         'clr-italic': 'hsl(0, 0%, 89%)',
         'clr-italic--dark': 'hsl(0, 0%, 60%)',
       },
       spacing: {
         'container-padding': '7.5vw',
       },
-      padding: {
-        mainBtnP: '1.25rem',
-      },
       backgroundImage: {
         'hero-not-found':
-          "linear-gradient(to right bottom, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65)), url('../src/imgs/not-found.webp')",
+          "linear-gradient(to right bottom, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65)), url('../src/imgs/not-found.jpg')",
         'hero-home':
           "linear-gradient(to right bottom, rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url('../src/imgs/home/hero.jpg')",
-      },
-      borderRadius: {
-        lg: '25px',
+        testimonials:
+          "linear-gradient(to right bottom, rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url('../src/imgs/home/testimonials-bkg.jpg')",
+        services: "url('../src/imgs/services/services-bkg.svg')",
       },
       boxShadow: {
         'card-shadow': '0px 0px 3px 1px rgb(0 0 0 / 18%)',
+        'process-shadow': ' 0px 3px 4px 0px rgba(0 0 0 / 12%)',
+        'numbers-shadow': '0px 0px 3px 1px rgb(0 0 0 / 7%)',
       },
-      backgroundColor: {
-        'opacity-white': 'rgba(255,255,255,0.1)',
+      dropShadow: {
+        'phone-shadow': '1px 1px 0px hsl(214, 100%, 29%)',
+        'section-shadow': '1px 1px 0px rgba(0, 0, 0, 0.6)',
       },
       translate: {
-        'negative-50': '-50%',
-        'negative-100': '-100%',
         'negative-50vw': '-50vw',
-        'negative-100vw': '-100vw',
       },
       maxWidth: {
         'rem-100': '100rem',
-        'rem-80': '80rem',
+        'rem-27': '27.5rem',
       },
       maxHeight: {
-        'rem-40': '30rem',
+        'rem-80': '80rem',
+      },
+      minHeight: {
+        'rem-55': '55rem',
       },
     },
   },
   plugins: [],
 };
-//
